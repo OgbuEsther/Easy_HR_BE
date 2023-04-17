0d81ce87deedd76d4466b58e2319bf76802fd1eb
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-04-17 16:39:55 +0100

    about to redeploy

---
+++ dist/src/model/admin/adminAttendance/AdminAttendance.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const adminAttendanceSchema = new mongoose_1.default.Schema({
+    setToken: {
+        type: String,
+    },
+    viewStaffAttendance: [
+        {
+            type: mongoose_1.default.Schema.Types.ObjectId,
+            ref: "StaffAttendance",
+        },
+    ],
+});
+const adminAttendanceModel = mongoose_1.default.model("Attendance", adminAttendanceSchema);
+exports.default = adminAttendanceModel;
