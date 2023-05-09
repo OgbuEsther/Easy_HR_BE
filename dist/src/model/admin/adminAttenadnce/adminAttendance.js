0873aeaef8abc1d4afa04ead26f002b7956f262e
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-05-09 08:49:53 +0100

    making huge changes

---
+++ dist/src/model/admin/adminAttenadnce/adminAttendance.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const AttendanceSchema = new mongoose_1.default.Schema({
+    date: {
+        type: String
+    },
+    clockIn: {
+        type: Boolean,
+        default: false
+    },
+    clockOut: {
+        type: Boolean,
+        default: false
+    },
+    message: {
+        type: String
+    },
+    time: {
+        type: String
+    },
+    token: {
+        type: String
+    },
+});
+const AdminAttendanceModel = mongoose_1.default.model("Attendance", AttendanceSchema);
+exports.default = AdminAttendanceModel;
