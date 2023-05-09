570e56eb93b854a5d6f6eb8adc264f697abdf019
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-05-09 19:09:27 +0100

    go hard or go home

--- src/controller/Performance/adminPerformance.ts
+++ src/controller/Performance/adminPerformance.ts
@@ -58,22 +58,46 @@ export const PerformanceMilestone = async (req: Request, res: Response) => {
 export const createMileStone = async (req: Request, res: Response) => {
   try {
     const { mileStone } = req.body;
-    const getAdmin = await adminAuth.findById(req.params.adminId);
 
-    const milestone = await mileStoneModel.create({
-      mileStone,
-    });
+    function getDaysInMonth(year: any, month: any) {
+      return new Date(year, month, 0).getDate();
+    }
 
-    await getAdmin?.createPerformanceMilestone.push(
-      new mongoose.Types.ObjectId(milestone?._id)
-    );
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const currentMonth = date.getMonth() + 1; // 👈️ months are 0-based
 
-    await getAdmin?.save();
+    // 👇️ Current Month
+    const daysInCurrentMonth = getDaysInMonth(currentYear, currentMonth);
+    console.log(daysInCurrentMonth);
 
-    return res.status(200).json({
-      message: "milestone created",
-      data: milestone,
-    });
+    //get actual date
+    const getCurrentDate = new Date().toLocaleDateString().split("")[2];
+
+    const getvalue:number = parseInt(getCurrentDate);
+
+    if (getvalue >= 1 && getvalue >= 4) {
+      const getAdmin = await adminAuth.findById(req.params.adminId);
+
+      const milestone = await mileStoneModel.create({
+        mileStone,
+      });
+
+    //   await getAdmin?.createPerformanceMilestone.push(
+    //     new mongoose.Types.ObjectId(milestone?._id)
+    //   );
+
+    //   await getAdmin?.save();
+
+      return res.status(200).json({
+        message: "milestone created",
+        data: milestone,
+      });
+    } else {
+      return res.status(400).json({
+        message: "it's past creation time ",
+      });
+    }
   } catch (error: any) {
     return res.status(400).json({
       message: "milestone not created",
@@ -83,67 +107,50 @@ export const createMileStone = async (req: Request, res: Response) => {
   }
 };
 
+export const enterStaffScore = async (req: Request, res: Response) => {
+  try {
+    const { staffScore } = req.body;
+    const createStaffScore = await rateModel.create({
+      adminScore: 0,
+      staffScore,
+      // date : Date.getDate()
+    });
 
-export const enterStaffScore = async(req:Request , res:Response) =>{
-    try {
+    return res.status(201).json({
+      message: "entered score sucessfully",
+      data: createStaffScore,
+    });
+  } catch (error: any) {
+    return res.status(400).json({
+      message: "failed to enter score",
+      data: error,
+      err: error.message,
+    });
+  }
+};
 
-        const {staffScore} = req.body
-        const createStaffScore = await rateModel.create({
-            adminScore : 0,
-            staffScore,
-            // date : Date.getDate()
-        })
+export const enterAdminScore = async (req: Request, res: Response) => {
+  try {
+    const { adminScore } = req.body;
+    const getRateModel = await rateModel.findById(req.params.rateId);
 
-        function getDaysInMonth(year:any, month:any) {
-            return new Date(year, month, 0).getDate();
-          }
-          
-          const date = new Date();
-          const currentYear = date.getFullYear();
-          const currentMonth = date.getMonth() + 1; // 👈️ months are 0-based
-          
-          // 👇️ Current Month
-          const daysInCurrentMonth = getDaysInMonth(currentYear, currentMonth);
-          console.log(daysInCurrentMonth)
+    const updateScore = await rateModel.findByIdAndUpdate(
+      getRateModel?._id,
+      {
+        adminScore,
+      },
+      { new: true }
+    );
 
-          const getCurrentDate = new Date().toLocaleDateString();
-
-          console.log(getCurrentDate)
-
-       
-
-        return res.status(201).json({
-            message : "entered score sucessfully",
-            data : createStaffScore
-        })
-
-    } catch (error:any) {
-        return res.status(400).json({
-            message : "failed to enter score",
-            data : error,
-            err : error.message
-        })
-    }
-}
-
-export const enterAdminScore = async(req:Request , res:Response)=>{
-    try {
-        const {adminScore} = req.body
-        const getRateModel = await rateModel.findById(req.params.rateId)
-
-        const updateScore = await rateModel.findByIdAndUpdate(getRateModel?._id , {
-            adminScore
-        } , {new :true})
-
-        return res.status(201).json({
-            message : "entered score sucessfully",
-            data : updateScore
-        })
-    } catch (error:any) {
-        return res.status(400).json({
-            message : "failed to enter score",
-            data : error,
-            err : error.message
-        })
-    }
-}
+    return res.status(201).json({
+      message: "entered score sucessfully",
+      data: updateScore,
+    });
+  } catch (error: any) {
+    return res.status(400).json({
+      message: "failed to enter score",
+      data: error,
+      err: error.message,
+    });
+  }
+};
