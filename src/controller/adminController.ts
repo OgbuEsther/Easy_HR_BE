811f55e570f9ba03ad179d61b7127f8bd8c38edf
Esther Uzoma Ogbu <ogbuuzoma413@gmail.com>
2024-04-03 13:19:45 +0100

    made some changes on the admin controller

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -1,6 +1,6 @@
 import adminAuth from "../model/admin/adminAuth";
 import mongoose from "mongoose";
-import express,{ NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
 
 import otpgenerator from "otp-generator";
@@ -15,7 +15,6 @@ import axios from "axios";
 import { SuperfaceClient } from "@superfaceai/one-sdk";
 const app = express();
 app.set("trust proxy", true);
-
 
 const sdk = new SuperfaceClient();
 
@@ -36,7 +35,7 @@ async function run(ip: any) {
           apikey: "41b7b0ed377c175c4b32091abd68d049f5b6b748b2bee4789a161d93",
         },
       },
-    },
+    }
   );
 
   // Handle the result
@@ -48,15 +47,11 @@ async function run(ip: any) {
   }
 }
 
-
 export const adminSignup = asyncHandler(
   async (req: Request, res: Response, next: NextFunction, ip: any) => {
     try {
- 
-
       // Use the profile
-     
-      
+
       const {
         companyname,
         email,
@@ -65,33 +60,30 @@ export const adminSignup = asyncHandler(
         walletNumber,
         token,
         OTP,
-       
       } = req.body;
 
       const genToken = crypto.randomBytes(32).toString("hex");
       const genOTP = crypto.randomBytes(2).toString("hex");
-     
-        if (!email || !yourName || !password) {
-          return res.status(400).json({
-            message: "please fill in the required fields",
-          });
-        }
-     
-      
+
+      if (!email || !yourName || !password) {
+        return res.status(400).json({
+          message: "please fill in the required fields",
+        });
+      }
 
       const salt = await bcrypt.genSalt(10);
       const hash = await bcrypt.hash(password, salt);
 
       const dater = Date.now();
-const defaultTime = "07:30:00 PM"
+      const defaultTime = "07:30:00 PM";
 
-let dataIP: any;
+      let dataIP: any;
 
-await axios.get("https://api.ipify.org/").then((res: any) => {
-  dataIP = res.data;
-});
+      await axios.get("https://api.ipify.org/").then((res: any) => {
+        dataIP = res.data;
+      });
 
-let realData: any = await run(dataIP);
+      let realData: any = await run(dataIP);
       const generateNumber = Math.floor(Math.random() * 78) + dater;
       const genCode = otpgenerator.generate(6, {
         upperCaseAlphabets: false,
@@ -109,9 +101,8 @@ let realData: any = await run(dataIP);
         token: genToken,
         OTP: genOTP,
         expectedClockIn: defaultTime,
-        latitude : realData?.latitude,
-        longitude : realData?.longitude,
-        
+        latitude: realData?.latitude,
+        longitude: realData?.longitude,
       });
 
       if (!admin) {
@@ -130,8 +121,6 @@ let realData: any = await run(dataIP);
         credit: 0,
         debit: 0,
       });
-    
-
 
       admin?.wallet.push(new mongoose.Types.ObjectId(createWallet?._id));
 
@@ -146,31 +135,32 @@ let realData: any = await run(dataIP);
         );
       }
 
-      verifyEmail(admin).then((res)=> console.log(res)).catch((err)=> console.log(err));
-     
-  
-  return res.status(200).json({
-    message: "Success",
-    data: admin,
-    result:  realData
+      verifyEmail(admin)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+
+      return res.status(200).json({
+        message: "Success",
+        data: admin,
+        result: realData,
       });
     } catch (error: any) {
       return res.status(400).json({
         message: "an error occurred while creating admin",
         data: error,
-        err : error.message
+        err: error.message,
       });
     }
   }
 );
 
-export const getIpAddress = async(req:Request , res:Response)=>{
+export const getIpAddress = async (req: Request, res: Response) => {
   try {
     let dataIP: any;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 //sign in
 export const adminSignin = async (req: Request, res: Response) => {
@@ -255,8 +245,7 @@ export const getOneAdmin = async (req: Request, res: Response) => {
       {
         path: "staffLeave",
       },
-     
-     
+
       {
         path: "viewLateStaff",
       },
@@ -266,7 +255,6 @@ export const getOneAdmin = async (req: Request, res: Response) => {
       {
         path: "PerformanceMilestone",
       },
-     
     ]);
 
     return res.status(200).json({
@@ -286,28 +274,22 @@ export const getOneAdmin = async (req: Request, res: Response) => {
 
 export const verifyUser = async (req: Request, res: Response) => {
   try {
-
-
     const admin = await adminAuth.findById(req.params.adminId);
 
     if (admin?.OTP !== "") {
       if (admin?.token !== "") {
-  
-          
+        await adminAuth.findByIdAndUpdate(
+          admin?._id,
+          {
+            token: "",
+            verified: true,
+          },
+          { new: true }
+        );
 
-            await adminAuth.findByIdAndUpdate(
-              admin?._id,
-              {
-                token: "",
-                verified: true,
-              },
-              { new: true }
-            );
-
-            return res.status(201).json({
-              data: admin,
-            });
-  
+        return res.status(201).json({
+          data: admin,
+        });
       } else {
         return res.status(400).json({
           message: "you have inputed a wrong otp",
@@ -326,42 +308,51 @@ export const verifyUser = async (req: Request, res: Response) => {
   }
 };
 
-
 //account settings
 
 export const updateAdmin = async (req: Request, res: Response) => {
   try {
-    const { expectedClockIn , expectedClockOut,companyname ,email ,yourName ,workingDays } = req.body;
+    const {
+      expectedClockIn,
+      expectedClockOut,
+      companyname,
+      email,
+      yourName,
+      workingDays,
+    } = req.body;
 
-    const getAdmin = await adminAuth.findById(req.params.adminId)
+    const getAdmin = await adminAuth.findById(req.params.adminId);
 
     // const getStaffDetails = await staffAuth.findById(req.params.staffId);
 
-    if(getAdmin){
+    if (getAdmin) {
       const update = await adminAuth.findByIdAndUpdate(
         getAdmin?._id,
-        { expectedClockIn , expectedClockOut,companyname ,email ,yourName, workingDays },
+        {
+          expectedClockIn,
+          expectedClockOut,
+          companyname,
+          email,
+          yourName,
+          workingDays,
+        },
         { new: true }
       );
-  
-  
-      return res.status(201).json({
-        message : "updated admin details successfully",
-        data : update
-      })
-    }else{
-      return res.status(400).json({
-        message : "admin not found",
-      
-      })
-    }
 
-    
-  } catch (error:any) {
+      return res.status(201).json({
+        message: "updated admin details successfully",
+        data: update,
+      });
+    } else {
+      return res.status(400).json({
+        message: "admin not found",
+      });
+    }
+  } catch (error: any) {
     return res.status(400).json({
       message: "couldn't update admin",
-      data : error,
-      error : error.message
+      data: error,
+      error: error.message,
     });
   }
 };
